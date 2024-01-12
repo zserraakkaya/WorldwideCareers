5927ffd83c758218518c7f1ae4edd7ab1849ae78
zserraakkaya <zserraakkaya2@gmail.com>
2024-01-12 14:57:09 +0100

    Homepage and jobs section added

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Information Technology Support Technician',
+        'location': 'Novara, Piedmont, Italy'
+    },
+    {
+        'id': 2,
+        'title': 'DevOps Engineer',
+        'location': 'Vienna, Austria'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html", jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
